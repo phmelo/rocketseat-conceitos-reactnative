f7382154cb8451e458f93f809ea21ed592bf0dca
Paulo Henrique <phmelomorais@gmail.com>
2020-05-31 22:39:45 -0300

    Challenge completed!

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from "./services/api";
 
 import {
   SafeAreaView,
@@ -11,8 +12,37 @@ import {
 } from "react-native";
 
 export default function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(()=>{
+    api.get('repositories').then(response => {
+      console.log(response.data);
+      setRepositories(response.data);
+    });
+  },[]);
+
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    const repoIndex = repositories.findIndex(repo => repo.id === id);
+    if (repoIndex < 0){
+      console.log(`Repository ${id} not found`);
+      return;
+    }
+    api.post(`repositories/${id}/like`).then(response => {
+      console.log(response.data);
+    });
+
+    let repositoriesNew = repositories;
+    const { url, title, techs, likes } = repositories[repoIndex];
+    const modifiedRepo = {id, url, title, techs, likes: likes + 1};
+    repositoriesNew[repoIndex] = modifiedRepo;
+    
+    setRepositories([...repositoriesNew]);
+    repositoriesNew=[];
+    
+    //setRepositories([...repositories.filter(r => r.id !== id),modifiedRepo]);
+    //console.log(repositories.splite(1))
   }
 
   return (
@@ -20,35 +50,37 @@ export default function App() {
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
         <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
+          <FlatList
+            data={repositories}
+            keyExtractor={repo => repo.id}
+            renderItem={({item}) => (
+              <>
+                <Text style={styles.repository}>{item.title}</Text>
+                <View style={styles.techsContainer}>
+                  <Text style={styles.tech}>ReactJS</Text>
+                  <Text style={styles.tech}>Node.js</Text>
+                </View>
+                <View style={styles.likesContainer}>
+                  <Text
+                    style={styles.likeText}
+                    testID={`repository-likes-${item.id}`}
+                  >
+                    {item.likes} curtidas
+                  </Text>
+                </View>
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => handleLikeRepository(item.id)}
+                  testID={`like-button-${item.id}`}
+                >
+                  <Text style={styles.buttonText}>Curtir</Text>
+                </TouchableOpacity>                
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
+              </>
 
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
+  
+            )}
+          />
         </View>
       </SafeAreaView>
     </>
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "http://172.29.220.74:3333",
 });
 
 export default api;
